d5c9133c9fff37fe4743fac0bf94f81ec6fb06de
JagaranAG2022 <jagaran@ambition.guru>
2023-02-07 17:03:06 +0545

    webview code upgrade

--- lib/src/widgets/iframe_mobile.dart
+++ lib/src/widgets/iframe_mobile.dart
@@ -6,6 +6,11 @@ import 'package:flutter_html/src/replaced_element.dart';
 import 'package:flutter_html/style.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:html/dom.dart' as dom;
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+
+// Import for iOS features.
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+// #enddocregion platform_imports
 
 /// [IframeContentElement is a [ReplacedElement] with web content.
 class IframeContentElement extends ReplacedElement {
@@ -30,17 +35,65 @@ class IframeContentElement extends ReplacedElement {
     return Container(
       width: width ?? (height ?? 150) * 2,
       height: height ?? (width ?? 300) / 2,
-      child: WebView(
-        initialUrl: src,
+      child: CustomWebViewWidget(
+        isSandboxMode: sandboxMode == null || sandboxMode == "allow-scripts",
         key: key,
-        javascriptMode: sandboxMode == null || sandboxMode == "allow-scripts"
-            ? JavascriptMode.unrestricted
-            : JavascriptMode.disabled,
+        url: src ?? '',
         navigationDelegate: navigationDelegate,
-        gestureRecognizers: {
-          Factory<VerticalDragGestureRecognizer>(() => VerticalDragGestureRecognizer())
-        },
       ),
+    );
+  }
+}
+
+class CustomWebViewWidget extends StatefulWidget {
+  final String url;
+  final bool isSandboxMode;
+  final NavigationDelegate? navigationDelegate;
+
+  const CustomWebViewWidget({
+    required this.url,
+    required this.isSandboxMode,
+    required Key key,
+    this.navigationDelegate,
+  }) : super(key: key);
+
+  @override
+  State<CustomWebViewWidget> createState() => _CustomWebViewWidgetState();
+}
+
+class _CustomWebViewWidgetState extends State<CustomWebViewWidget> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    final PlatformWebViewControllerCreationParams params =
+        const PlatformWebViewControllerCreationParams();
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(params);
+    controller
+      ..setJavaScriptMode(widget.isSandboxMode
+          ? JavaScriptMode.unrestricted
+          : JavaScriptMode.disabled)
+      ..loadRequest(Uri.parse(widget.url));
+
+    if (widget.navigationDelegate != null) {
+      controller..setNavigationDelegate(widget.navigationDelegate!);
+    }
+
+    _controller = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebViewWidget(
+      controller: _controller,
+      key: widget.key,
+      gestureRecognizers: {
+        Factory<VerticalDragGestureRecognizer>(
+          () => VerticalDragGestureRecognizer(),
+        )
+      },
     );
   }
 }
