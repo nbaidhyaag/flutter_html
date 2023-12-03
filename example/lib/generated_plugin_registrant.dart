0d178ad8f365199cf293dc5b8bb0c7549d9a26b1
Sasen Tamrakar <sasen.tamrakar@yajtechnologies.com>
2023-12-03 11:25:52 +0545

    chewie updated

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -4,10 +4,9 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:video_player_web/video_player_web.dart';
 import 'package:wakelock_web/wakelock_web.dart';
-
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
